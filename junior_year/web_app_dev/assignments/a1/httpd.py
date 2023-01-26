d78e718c0a766f86d654e56f5e4e2658080a1a10
jed-richards <jed22richards@gmail.com>
2023-01-26 14:48:06 -0600

    WAD assignment1 stuff

---
+++ junior_year/web_app_dev/assignments/a1/httpd.py
@@ -0,0 +1,52 @@
+from http.server import *
+from sys import argv      # allows command line arguments
+import time
+
+# This allows the use of: python3 python-httpd.py <PORT NUMBER>
+# Otherwise the default is port 8000
+if len(argv) >= 2:
+    port = int(argv[1])
+else:
+    port = 8000
+
+
+class MyHTTPRequestHandler(SimpleHTTPRequestHandler):
+    def __init__(self, request, client_address, server):
+        SimpleHTTPRequestHandler.__init__(self, request, client_address, server)
+
+    def do_GET(self):
+        print("Serving %s " % self.path)
+        print("Serving from %s " % self.translate_path(self.path))
+
+        if self.path.startswith('/dyn/'):
+            file = self.translate_path(self.path)   # does this work??
+            print(file)
+
+            with open(file, 'r') as inf, open('newfile.html', 'w') as outf:
+                #for line in inf:
+                    #if '{{sysdate}}' in line:
+
+                    ###########################################################
+                    # figure out how to only change {{sysdate}} not everything
+                    # on the line
+                    # time.asctime()
+                    ###########################################################
+
+                outf.write(inf.read().replace('{{sysdate}}', time.asctime()))
+            self.path = '/newfile.html'
+            print(self.path)
+            print(self)
+            
+        SimpleHTTPRequestHandler.do_GET(self)
+
+
+def run(server_class=HTTPServer, handler_class=MyHTTPRequestHandler):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+print(f'Starting HTTP server on port {port}.')
+run()
+print('Running...')
+
+# Run and visit http://localhost:8000/ in your web browser
